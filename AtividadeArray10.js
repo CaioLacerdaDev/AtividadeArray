d179f537bd772c312fbefa66a5ec1aea99c1d156
CaioLacerdaDev <caiolacerda.tds1.senai@gmail.com>
2024-03-17 13:09:14 -0300

    Array CORREÇAO Slice()

--- AtividadeArray10.js
+++ AtividadeArray10.js
@@ -4,7 +4,9 @@
 let numeros = [1, 2, 3, 4, 5];
 let subArray = numeros.slice(1, 3); // cria uma cópia de [2, 3]
 // subArray é igual a [2, 3]
+console.log(subArray)
 
 let letras = ["a", "b", "c", "d", "e"];
 let subArrayLetras = letras.slice(2); // cria uma cópia de ['c', 'd', 'e']
 // subArrayLetras é igual a ['c', 'd', 'e']
+console.log(subArrayLetras)
