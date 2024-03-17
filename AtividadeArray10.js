07f76fff82df9f8702b3ac13736e4fe60e192034
CaioLacerdaDev <caiolacerda.tds1.senai@gmail.com>
2024-03-17 13:06:24 -0300

    Array Slice()

---
+++ AtividadeArray10.js
@@ -0,0 +1,10 @@
+//slice() retorna uma cópia superficial de uma porção de um array
+//em um novo array. Não altera o array original.
+
+let numeros = [1, 2, 3, 4, 5];
+let subArray = numeros.slice(1, 3); // cria uma cópia de [2, 3]
+// subArray é igual a [2, 3]
+
+let letras = ["a", "b", "c", "d", "e"];
+let subArrayLetras = letras.slice(2); // cria uma cópia de ['c', 'd', 'e']
+// subArrayLetras é igual a ['c', 'd', 'e']
