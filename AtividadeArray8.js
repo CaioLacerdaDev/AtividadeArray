ce60ea626da2ace9f43935ff107b62b9c180f40b
CaioLacerdaDev <caiolacerda.tds1.senai@gmail.com>
2024-03-17 12:44:48 -0300

    Array lastindexOf()

---
+++ AtividadeArray8.js
@@ -0,0 +1,13 @@
+//lastIndexOf() retorna o último índice em que um elemento específico 
+//pode ser encontrado no array, ou -1 se o elemento não estiver 
+//presente.
+
+let numeros = [10, 20, 30, 40, 30, 50];
+let indicetrinta = numeros.lastIndexOf(30);
+// indicetrinta é igual a 4
+console.log(indicetrinta)
+
+let letras = ["a", "b", "c", "a", "d"];
+let indiceA = letras.lastIndexOf("a");
+// indiceA é igual a 3
+console.log(indiceA)
