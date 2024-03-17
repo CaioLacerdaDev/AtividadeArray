1e4083204fbe3c96b4888f2e72cbe2839681af28
CaioLacerdaDev <caiolacerda.tds1.senai@gmail.com>
2024-03-17 12:02:59 -0300

    Array Reverse()

---
+++ AtividadeArray5.js
@@ -0,0 +1,12 @@
+//reverse() inverte a ordem dos elementos de um array 
+
+
+let numeros = [1, 2, 3, 4, 5];
+numeros.reverse();
+// Agora, numeros contém [5, 4, 3, 2, 1]
+console.log(numeros)
+
+let letras = ["a", "b", "c", "d", "e"];
+letras.reverse();
+// Agora, letras contém ['e', 'd', 'c', 'b', 'a']
+console.log(letras)
