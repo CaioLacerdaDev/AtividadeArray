df17d60528dd9bc2990aa511a47f4b86694ade13
CaioLacerdaDev <caiolacerda.tds1.senai@gmail.com>
2024-03-17 11:37:33 -0300

    Array Concat()

---
+++ AtividadeArray2.js
@@ -0,0 +1,13 @@
+//concat() combina dois ou mais arrays, criando um novo array.
+
+let numeros1 = [1, 2];
+let numeros2 = [3, 4];
+let concatenados = numeros1.concat(numeros2);
+// concatenados é igual a [1, 2, 3, 4]
+console.log (concatenados)
+
+let frutas1 = ["maçã", "banana"];
+let frutas2 = ["laranja", "morango"];
+let exibir = frutas1.concat(frutas2);
+// exibir é igual a ['maçã', 'banana', 'laranja', 'morango']
+console.log (exibir)
