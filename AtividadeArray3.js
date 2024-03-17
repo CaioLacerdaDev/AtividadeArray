ef429d157d1ad4d90e597785d3b7f3017829eb2c
CaioLacerdaDev <caiolacerda.tds1.senai@gmail.com>
2024-03-17 11:50:21 -0300

    Array Sort()

---
+++ AtividadeArray3.js
@@ -0,0 +1,13 @@
+//sort() organiza os elementos de um array em ordem 
+//alfabética (quando aplicado a strings) ou em ordem 
+//crescente (quando aplicado a números) 
+
+let frutas = ['laranja', 'maçã', 'banana'];
+frutas.sort();
+//frutas contém ['banana', 'laranja', 'maçã']
+console.log (frutas) 
+
+let numeros = [3, 1, 5, 2, 4];
+numeros.sort();
+//numeros contém [1, 2, 3, 4, 5]
+console.log (numeros)
