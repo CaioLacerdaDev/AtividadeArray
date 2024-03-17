eac15689f9dc4db4506983f45cbdfcd904f867e8
CaioLacerdaDev <caiolacerda.tds1.senai@gmail.com>
2024-03-17 11:28:21 -0300

    Array includes()

---
+++ AtividadeArray1.js
@@ -0,0 +1,14 @@
+//includes() verifica se um array contém um determinado 
+//elemento e retorna true ou false caso verdadeiro ou falso
+
+
+let numeros = [1, 2, 3, 4, 5];
+let verificar = numeros.includes(3);
+// "verificar" é igual a true
+console.log (verificar)
+
+let frutas = ["maçã", "banana", "laranja"];
+let verifica = frutas.includes("pera");
+// "verifica" é igual a false
+console.log (verifica)
+
